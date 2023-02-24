c5fec07e85cc141d2b0acd0e16f8f531db45289c
koshtirushikesh <koshtirushikesh3@gmail.com>
2023-02-24 16:42:02 +0530

    [Rushi]: Anagram program

---
+++ AlgorithmPrograms/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmPrograms
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            start:
+            Console.WriteLine("1.Anagram");
+            Console.Write("Enter Option");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    Anagram anagram = new Anagram();
+                    anagram.Check("heart", "earth");
+                    break;
+            }
+            Console.WriteLine("Press {y]: continue or [n]:Exist");
+            char run = Convert.ToChar(Console.ReadLine());
+            if (run == 'y')
+                goto start;
+        }
+    }
+}
